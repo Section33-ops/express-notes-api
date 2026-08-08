976b41cdf1ae6cd9352f69266fffc8e1a02ba225
Saeed Mamani <228374580+Section33-ops@users.noreply.github.com>
2026-08-08 14:51:00 -0400

    feat: add middleware to log requests

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ const port = 8080;
 
 let notes = [];
 
+function logRequest(req, res, next) {
+  console.log(`${req.method} request recieved from ${req.originalUrl}!`);
+  next();
+}
+
 app.use(express.json());
 
-app.get('/notes', (req, res) => {
+app.get('/notes', logRequest, (req, res) => {
   res.json(notes);
 });
 
-app.post('/notes', (req, res) => {
+app.post('/notes', logRequest, (req, res) => {
   const { title, content } = req.body;
   notes.push({ id: notes.length + 1, title: title, content: content });
   res.status(200).json({ message: 'Note added successfully' });
 });
 
-app.get('/notes/find', (req, res) => {
+app.get('/notes/find', logRequest, (req, res) => {
   const searchKeyword = req.query.search.toLowerCase();
   const foundNotes = notes.filter((note) =>
     note.title.toLowerCase().includes(searchKeyword),
@@ -25,13 +30,13 @@ app.get('/notes/find', (req, res) => {
   res.json(foundNotes);
 });
 
-app.get('/notes/:id', (req, res) => {
+app.get('/notes/:id', logRequest, (req, res) => {
   const findId = Number(req.params.id);
   const findNote = notes.filter((note) => note.id === findId);
   res.json(findNote);
 });
 
-app.put('/notes/:id', (req, res) => {
+app.put('/notes/:id', logRequest, (req, res) => {
   const { content } = req.body;
   const findId = Number(req.params.id);
   const updateNotes = notes.map((note) =>
@@ -43,7 +48,7 @@ app.put('/notes/:id', (req, res) => {
   res.json(notes);
 });
 
-app.delete('/notes/:id', (req, res) => {
+app.delete('/notes/:id', logRequest, (req, res) => {
   const findId = Number(req.params.id);
   let idNum = 0;
   const updateNotes = notes
